9bf8dd55dfd40a0892efaa4207ab251714c5f04b
HurstJake1024 <hurstjake1024@gmail.com>
2022-06-10 21:22:59 -0400

    1st commit

---
+++ src/gitReview/gitReview01.java
@@ -0,0 +1,12 @@
+package gitReview;
+
+public class gitReview01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("today we learn github");
+		System.out.println("today is a great day");
+
+	}
+
+}
